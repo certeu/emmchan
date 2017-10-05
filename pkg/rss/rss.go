e3aed94aa8b2a8f858f13f6d611a897539510ce2
Alexandru Ciobanu <alex@cert.europa.eu>
2017-10-06 01:26:31 +0200

    Ensure we can read other charsets

--- pkg/rss/rss.go
+++ pkg/rss/rss.go
@@ -3,6 +3,10 @@ package rss
 import (
 	"bytes"
 	"encoding/xml"
+	"fmt"
+	"io"
+
+	"golang.org/x/text/encoding/charmap"
 )
 
 type RSSFeed struct {
@@ -14,11 +18,12 @@ type RSSChannel struct {
 	XMLName       xml.Name `xml:"channel"`
 	URL           string   `xml:"-"`
 	Title         string   `xml:"title"`
-	Link          string   `xml:"link"`
+	Link          string   `xml:"-"`
+	LinkSlice     []string `xml:"link"`
 	Description   string   `xml:"description"`
 	Language      string   `xml:"language"`
 	PubDate       string   `xml:"pubDate"`
-	LastBuildDate string   `xml:"LastBuilDate"`
+	LastBuildDate string   `xml:"lastBuildDate"`
 	Items         []Item
 }
 
@@ -35,8 +40,26 @@ type Item struct {
 func NewRSSFeed(buf []byte) (*RSSFeed, error) {
 	f := RSSFeed{}
 	d := xml.NewDecoder(bytes.NewReader(buf))
+	d.CharsetReader = makeCharsetReader
 	if err := d.Decode(&f); err != nil {
 		return nil, err
 	}
+	for _, l := range f.Channel.LinkSlice {
+		if l != "" {
+			f.Channel.Link = l
+			break
+		}
+	}
 	return &f, nil
+}
+
+func makeCharsetReader(charset string, input io.Reader) (io.Reader, error) {
+	if charset == "ISO-8859-1" || charset == "Windows-1252" {
+		// Windows-1252 is a superset of ISO-8859-1, so should do here
+		return charmap.Windows1252.NewDecoder().Reader(input), nil
+	}
+	if charset == "Windows-1255" {
+		return charmap.Windows1255.NewDecoder().Reader(input), nil
+	}
+	return nil, fmt.Errorf("Unknown charset: %s", charset)
 }
